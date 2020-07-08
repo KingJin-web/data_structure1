4032b0f6bbcc9a0c2281d2370d32d47f4f790d74
12613 <1261337209@qq.com>
2020-07-08 15:47:07 +0800

    数据结构课设
    Signed-off-by: 12613 <1261337209@qq.com>

--- src/dao/test.java
+++ src/dao/test.java
@@ -9,12 +9,13 @@ import java.util.Map;
 
 public class test {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-//        ArrayList<Student> list = s.selectAllStudent();
-//        System.out.println("姓名   学号  英语  历史   高数   体育  数据结构 总分");
-//        for(int i = 0 ; i<list.size();i++){
-//            System.out.println(list.get(i));
-//        }
+        StudentDao s =new StudentDaoImp();
+        ArrayList<Student> list = s.selectAllStudent();
+        System.out.println("姓名   学号  英语  历史   高数   体育  数据结构 总分");
+        for(int i = 0 ; i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        //输出全部成绩
 //        System.out.println("姓名   学号  英语   历史  高数   体育  数据结构 总分");
 //        ListSort L = new ListSort();
 //        L.addStudent("张  三", "0005", 99, 56, 98, 36, 18);
--- src/file/SaveFile.java
+++ src/file/SaveFile.java
@@ -1,8 +1,41 @@
 package file;
 
+import bean.Student;
+import dao.StudentDao;
+import dao.StudentDaoImp;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 12613
  * ±£´æÎÄ¼þ
  */
 public class SaveFile {
+    private void save() throws SQLException {
+        StudentDao studentDao =new StudentDaoImp();
+        //ArrayList<Student> list = s.selectAllStudent();
+        List<Student> list  = studentDao.selectAllStudent();
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("d:\\b.txt"));
+            for (Student s : list) {
+                bw.write(String.valueOf(s));
+                bw.newLine();
+                bw.flush();
+            }
+            bw.close();
+        } catch ( IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        SaveFile saveFile =new SaveFile();
+        saveFile.save();
+    }
 }
